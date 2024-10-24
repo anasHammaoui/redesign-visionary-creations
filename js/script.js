7ae5f59874c61008af7f315c72c355802711fe4b
anashm <anashammaoui07@gmail.com>
2024-10-24 22:27:24 +0200

    finishig the project

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ var menuIcon = document.querySelector('.nav-icon');
 var navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     navbar.classList.toggle('hidden');
+// hide about services + fix the homepage when click the berger menu
+    document.querySelector('.body').classList.toggle('overflow-hidden');
+    document.querySelector('.about').classList.toggle('hidden');
+    
+
 }
 // intro video
 document.querySelector('.intro').onclick = () => {
