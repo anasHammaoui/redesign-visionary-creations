8749e8c050c35640cbf9aee2c08d46289652d8b8
anashm <anashammaoui07@gmail.com>
2024-10-25 10:09:49 +0200

    add scroll

--- js/script.js
+++ js/script.js
@@ -6,13 +6,42 @@ menuIcon.onclick = () => {
 // hide about services + fix the homepage when click the berger menu
     document.querySelector('.body').classList.toggle('overflow-hidden');
     document.querySelector('.about').classList.toggle('hidden');
-    
-
 }
 // intro video
 document.querySelector('.intro').onclick = () => {
     document.querySelector('.intro-video').classList.remove('hidden');
+    document.querySelector('.body').classList.add('overflow-hidden');
 }
 document.querySelector('.intro-video').onclick = () => {
     document.querySelector('.intro-video').classList.add('hidden');
+    document.querySelector('.body').classList.remove('overflow-hidden');
 }
+// smooth scrool
+// home
+document.querySelector('.home').addEventListener('click',()=> {
+    document.querySelector('#home').scrollIntoView({behavior:'smooth'});
+})
+// about
+document.querySelector('.about').addEventListener('click',()=> {
+    document.querySelector('#about').scrollIntoView({behavior:'smooth'});
+})
+// service
+document.querySelector('.service').addEventListener('click',()=> {
+    document.querySelector('#service').scrollIntoView({behavior:'smooth'});
+})
+// portfolio
+document.querySelector('.portfolio').addEventListener('click',()=> {
+    document.querySelector('#portfolio').scrollIntoView({behavior:'smooth'});
+})
+// team
+document.querySelector('.team').addEventListener('click',()=> {
+    document.querySelector('#team').scrollIntoView({behavior:'smooth'});
+})
+// testimonials
+document.querySelector('.testimonials').addEventListener('click',()=> {
+    document.querySelector('#testimonials').scrollIntoView({behavior:'smooth'});
+})
+// blog
+document.querySelector('.blog').addEventListener('click',()=> {
+    document.querySelector('#blog').scrollIntoView({behavior:'smooth'});
+})
